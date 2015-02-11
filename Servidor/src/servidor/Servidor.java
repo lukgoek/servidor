1ff970b25efc9eb4e64034696662b3a004de9427
Lugo Aguilar Humberto <fimaz@Multiplataforma11.local>
2015-02-11 11:37:15 -0700

    *se creo el proyecto servidor para conexiones

---
+++ Servidor/src/servidor/Servidor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author fimaz
+ */
+public class Servidor {
+
+   
+    
+    public static void main(String[] args) {
+        Server obj = new Server();
+        obj.conexion(5555);
+    }
+    
+}
